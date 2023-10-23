f4360dce9f986f42230e1faecc6645c7051d81e6
Oleg Pyatin <opyatin@i-neti.ru>
2023-10-23 20:29:54 +0300

    Add test database feeding script

---
+++ files/test_data_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO "user" VALUES (default, 'test100@yandex.ru', 'John', 25, 'm', null, '89993006050');
+INSERT INTO "user" VALUES (default, 'test205@mail.ru', 'Daniel', 28, 'm', '1995-05-02', '85552001040');
+INSERT INTO "user" VALUES (default, 'test310@gmail.com', 'Anthony', 32, 'm', null, '89005551125');
+INSERT INTO "user" VALUES (default, 'test890@yandex.ru', 'Jenny', 34, 'w', '1989-03-15', '89203004000');
